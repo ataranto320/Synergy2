ea58744f48e1e8087cacaa603a2df7903c50b26d
keenan560 <keenan560@gmail.com>
2019-06-12 19:34:50 -0400

    added jumbotron spinner state dropdown 3 FBI api calls zillow but its in xml

--- assets/app.js
+++ assets/app.js
@@ -21,17 +21,19 @@ var harp = new Audio("assets/Harp-sound-effect.mp3");
 // start point
 var i = 0;
 var images = [];
-var time = 5000;
+var time = 10000;
 
 //img lists
-images[0] = 'assets/images/cityHD.jpg';
-images[1] = 'assets/images/townHD.jpg';
-images[2] = 'assets/images/village.jpg';
-images[3] = 'assets/images/beachhouse.jpg';
-images[4] = 'assets/images/oldworld.jpg';
-images[5] = 'assets/images/southerntrees.jpg';
-images[6] = 'assets/images/countryside.jpg';
 
+images[0] = 'assets/images/sunset.jpg';
+images[1] = 'assets/images/village.jpg';
+images[2] = 'assets/images/beachhouse.jpg';
+images[3] = 'assets/images/oldworld.jpg';
+images[4] = 'assets/images/southerntrees.jpg';
+images[5] = 'assets/images/countryside.jpg';
+images[6] = 'assets/images/winter.jpg';
+images[7] = 'assets/images/townHD.jpg';
+images[8] = 'assets/images/cityHD.jpg';
 
 
 
@@ -52,7 +54,7 @@ window.onload = changeImg;
 
 // Calls to FBI crime data API
 var startYear = "2010";
-var endYear = "2010";
+var endYear = "2017";
 
 function fbiCall3() {
     var offense = 'burglary';
@@ -68,13 +70,13 @@ function fbiCall3() {
         for (var i = 0; i < response.results.length; i++) {
             count += response.results[i].actual;
         }
-        $("#crime-data").append(`<p class='fbi mx-2 mt-2'>Total of ${count} ${offense} cases reported from ${location} from  ${startYear} to ${endYear}</p></div></div>`);
+        $("#crime-data").append(`<h1 class='fbi mx-2 my-5'>Total of ${count} ${offense} cases reported in ${location} from  ${startYear} to ${endYear}</h1></div></div>`);
 
     })
 }
 
 function fbiCall2() {
- 
+
     var offense = 'rape';
     var location = $("#state").val().trim();
     queryURL = "https://cors-anywhere.herokuapp.com/https://api.usa.gov/crime/fbi/sapi/api/summarized/state/" + location + "/" + offense + "/" + startYear + "/" + endYear + "?api_key=ChwrQihADYg80bXGfi0547Dvxtx511wXFSmx7nYm";
@@ -89,7 +91,7 @@ function fbiCall2() {
         for (var i = 0; i < response.results.length; i++) {
             count += response.results[i].actual;
         }
-        $("#crime-data").append(`<p class='fbi mx-2 mt-2'>Total of ${count} ${offense} cases reported from ${location} from  ${startYear} to ${endYear}</p>`);
+        $("#crime-data").append(`<h1 class='fbi mx-2 my-5'>Total of ${count} ${offense} cases reported in ${location} from  ${startYear} to ${endYear}</h1>`);
 
     })
 }
@@ -110,7 +112,7 @@ function fbiCall() {
         for (var i = 0; i < response.results.length; i++) {
             count += response.results[i].actual;
         }
-        $(".results-card").append(`<div class='card'><div class='card-header text-white bg-primary'>FBI Crime Stats</div><div id="crime-data"class=''card-body text-center> <p class='fbi mx-2 mt-2'>Total of ${count} ${offense} cases reported from ${location} from  ${startYear} to ${endYear}</p></div></div>`);
+        $(".results-card").append(`<div class='card'><div class='card-header text-white bg-primary'>FBI Crime Data Explorer</div><div id="crime-data"class=''card-body text-center> <h1 class='fbi mx-2 my-5'>Total of ${count} ${offense} cases reported in ${location} from  ${startYear} to ${endYear}</h1></div></div>`);
         $("#spinner").hide();
     })
 }
@@ -122,21 +124,25 @@ function zillowCall() {
     var apiKey = "X1-ZWz1h4nz2xuyvf_aovt1";
     queryURL = "https://cors-anywhere.herokuapp.com/http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz1h4nz2xuyvf_aovt1&state=NY&output='json'";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET",
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        // dataType: 'jsonp'
 
-        }).then(function (response) {
-            console.log(response);
-        })
+    }).then(function (response) {
+        console.log(response);
+    })
+}
+
+function clear() {
+   $('.results-card').empty(); 
 }
 
 
-
 $(document).on("click", "#search", function (event) {
-   
+
     event.preventDefault();
-    
+
     harp.play();
     var state = $("#state").val().trim();
     var age = $("#age").val().trim();
@@ -148,26 +154,34 @@ $(document).on("click", "#search", function (event) {
     var newSearch = {
         state: state,
         age: age,
-        status: status,
+        mstatus: status,
         kids: kids,
         job: job
     }
 
     console.log(newSearch);
 
+
     database.ref().push(newSearch);
+
+    $(".results-card").append(`<div class='jumbotron bg-primary text-white col-6'><p class=''>Your Profile</p><p>You are ${age} years old</p><p>You are ${status}</p><p>You have ${kids} children</p><p>You have are a ${job} individual</p><p>Your desired state to live is ${state}</p></div>`);
 
     fbiCall();
     fbiCall2();
     fbiCall3();
     zillowCall();
 
-    $("#city").val("");
-    $("#age").val("");
-    $("#status").val("");
-    $("#kids").val("");
+
+    // $("#age").val("");
+    // $("#state").val("");
+    // $("#status").val("");
+    // $("#kids").val("");
+    // $("#job").val("");
+
 })
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("#spinner").hide();
 })
+
+$(document).on("click", "#clear", clear);
